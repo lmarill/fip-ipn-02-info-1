daad8c9aae9ad9096291b74f7c724dc496470889
Guillaume-itii <gaupert@itii-normandie.fr>
2022-04-06 14:33:31 +0200

    branch : Chat -Dernière update stable de chat

--- td-3/Chat.py
+++ td-3/Chat.py
@@ -20,7 +20,7 @@ class Animal:
         self._nom = nom
         self._classification = classification
         self._isCute = cutness
-        print("L'animal : " + self._nom + " à été crée |type : " + str(self._classification))
+        # print("L'animal : " + self._nom + " à été crée |type : " + str(self._classification))
 
     @property
     def classification(self):
@@ -38,25 +38,16 @@ class Animal:
 class Chat(Animal):
     def __init__(self, nom, classification):
         super().__init__(nom, classification, True)
-        print("Le chat : " + self._nom + " à été crée |type : " + str(self._classification))
+        # print("Le chat : " + self._nom + " à été crée |type : " + str(self._classification))
 
 
 class Chien(Animal):
     def __init__(self, nom, classification):
         super().__init__(nom, classification)
-        print("Le chien : " + self._nom + " à été crée |type : " + str(self._classification))
+        # print("Le chien : " + self._nom + " à été crée |type : " + str(self._classification))
 
 
 animal = Animal("Osama", Classification.MAMIFERE)
 chat = Chat("Minou", Classification.MAMIFERE)
 chien = Chien("Rheun", Classification.MAMIFERE)
 
-if chat.isCute:
-    print(chat.nom + " est mignon")
-else:
-    print(chat.nom + " est pas mignon")
-
-if chien.isCute:
-    print(chien.nom + " est mignon")
-else:
-    print(chien.nom + " est pas mignon")
