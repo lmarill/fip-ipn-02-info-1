43b2781a39840d3a93edc77d3779f82ab4167af5
Guillaume-itii <gaupert@itii-normandie.fr>
2022-04-06 14:12:20 +0200

    branch : Chat -Dernière update stable de chat

--- td-3/Chat.py
+++ td-3/Chat.py
@@ -2,34 +2,44 @@ from enum import Enum
 
 
 class Classification(Enum):
-    POISSON     = 0
-    INSECTE     = 1
-    OISEAU      = 2
-    MAMIFERE    = 3
-    AMPHIBIEN   = 4
-    REPTILE     = 5
-    IVERTEBRER  = 6
+    POISSON = 0
+    INSECTE = 1
+    OISEAU = 2
+    MAMIFERE = 3
+    AMPHIBIEN = 4
+    REPTILE = 5
+    IVERTEBRER = 6
 
 
 class Animal:
     _nom = ""
     _classification = None
+    _isCute = False
 
-    def __init__(self, nom, classification):
+    def __init__(self, nom, classification, cutness=False):
         self._nom = nom
         self._classification = classification
+        self._isCute = cutness
         print("L'animal : " + self._nom + " à été crée |type : " + str(self._classification))
 
     @property
     def classification(self):
         return self._classification
 
+    @property
+    def isCute(self):
+        return self._isCute
+
+    @property
+    def nom(self):
+        return self._nom
+
 
 class Chat(Animal):
-
     def __init__(self, nom, classification):
-        super().__init__(nom, classification)
+        super().__init__(nom, classification, True)
         print("Le chat : " + self._nom + " à été crée |type : " + str(self._classification))
+
 
 class Chien(Animal):
 
@@ -38,6 +48,16 @@ class Chien(Animal):
         print("Le chien : " + self._nom + " à été crée |type : " + str(self._classification))
 
 
-chat = Animal("Osama", Classification.MAMIFERE)
+animal = Animal("Osama", Classification.MAMIFERE)
 chat = Chat("Minou", Classification.MAMIFERE)
-chat = Chien("Rheun", Classification.MAMIFERE)
+chien = Chien("Rheun", Classification.MAMIFERE)
+
+if chat.isCute:
+    print(chat._nom + " est mignon")
+else:
+    print(chat._nom + " est pas mignon")
+
+if chien.isCute:
+    print(chien._nom + " est mignon")
+else:
+    print(chien._nom + " est pas mignon")
